81f7f66fe881f180ef0114ce8b43cef26509a3b3
mohap710 <Mohap710@gmail.com>
2022-07-24 08:07:18 +0200

    Handle Mongoose Errors ( Cast, Duplicate , Validation )

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,26 @@
-export const errorHandler = (err, req, res, next)=>{
-    console.log(err.stack.red)
+import { ErrorResponse } from "../utils/errorResponse.js";
 
-    res.status(err.code || 500).json({
-        success:false,
-        error:err.message || "Server Error"
-    })
-}
+export const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message
+  // Mongoose bad ObjectId
+  if(err.name === "CastError"){
+    const message = `Resource with the id of ${err.value} Not found`;
+    error = new ErrorResponse(404,message);
+  }
+  // Duplicate Value Entered 
+  if(err.code == 11000){
+    const message = `Duplicate value has been entered`;
+    error = new ErrorResponse(400, message);
+  }
+ // Mongoose Validation Error   
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(error => error.message)
+    error = new ErrorResponse(400, message);
+  } 
+  res.status(error.code || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+
+  });
+};
