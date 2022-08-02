506fc3f082d7bbbfb408ab159a2fc9983320e1d9
mohap710 <Mohap710@gmail.com>
2022-08-02 11:04:55 +0200

    use the CourseRoutes in the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotEnv from 'dotenv'
 import connectToDB  from './config/db.js'
 import bootcampRoutes from './routes/bootcamps.js'
+import courseRoutes from './routes/courses.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 
 dotEnv.config({ path: './config/config.env' })
@@ -19,5 +20,6 @@ const port = process.env.PORT || 5000
 // Mounting Routes
 
 app.use("/bootcamps", bootcampRoutes).use(errorHandler);
+app.use("/courses", courseRoutes).use(errorHandler);
 
 app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode and listening on port ${port}!`.bgGreen.black.bold))
