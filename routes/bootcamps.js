8fa63aaa17cb5cd18383cb0f302a6a22ac69de84
mohap710 <Mohap710@gmail.com>
2022-08-02 11:03:35 +0200

    Add Re-route to courses controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,10 +7,17 @@ import {
     deleteBootcamps,
     updateBootcamps
 }
-    from '../controllers/bootcamps.js'
+    from '../controllers/bootcamps.js';
 
+// Include other resources Routers
+import coursesRouter from "./courses.js";
+    
 const router = express.Router()
 
+// Re-route to other resources
+router.use("/:bootcampId/courses", coursesRouter);
+
+// Bootcamps Routes
 router.route('/')
     .get(getBootcamps)
     .post(createNewBootcamps)
