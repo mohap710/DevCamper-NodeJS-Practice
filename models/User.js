e325ab2c00efedd32f37be3a9aef0ad57d8a16cd
mohap710 <Mohap710@gmail.com>
2022-08-12 09:33:34 +0200

    Generate and hash Reset Password token Method

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -10,49 +11,68 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true,"Please add an email."],
-    unique:true,
+    required: [true, "Please add an email."],
+    unique: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please enter a valid Email",
     ],
   },
-  password:{
-    type:String,
-    required:[true,"Please enter a password"],
-    minLength:[6,"Password can not be less than 6 characters"],
-    select:false
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minLength: [6, "Password can not be less than 6 characters"],
+    select: false,
   },
-  role:{
-    type:String,
-    enum:["user","publisher"],
-    default:"user"
+  role: {
+    type: String,
+    enum: ["admin","user", "publisher"],
+    default: "user",
   },
-  resetPasswordToken:String,
-  resetPasswordExpire:Date,
-  createdAt:{
-    type:Date,
-    default:Date.now
-  }
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Hash Password using bcrypt
-UserSchema.pre("save", async function(next){
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
-})
+});
 
 // Sign JWT
-UserSchema.methods.signJWT = function(){
-  return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-    expiresIn:process.env.JWT_EXPIRE
-  })
-}
+UserSchema.methods.signJWT = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 // Check if entered password matches the hashed Password
-UserSchema.methods.comparePassword = async function(enteredPassword){
-  return await bcrypt.compare(enteredPassword,this.password);
-}
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash Reset Password token
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash the Token and set the resetPassword Field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set Expire Date for the Token => 10 mins
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
 
 export default mongoose.model("User", UserSchema);
