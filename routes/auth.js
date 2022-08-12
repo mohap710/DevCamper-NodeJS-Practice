68ac2c5af940b7e44f100ed93c562f31f8bef2cd
mohap710 <Mohap710@gmail.com>
2022-08-12 09:35:49 +0200

    Forget password , reset password, update Details , and update password Routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { register,login,getMe } from "../controllers/auth.js";
+import {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+} from "../controllers/auth.js";
 
 // Middlewares
 import { protect } from "../middlewares/auth.js";
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register",register)
-router.post("/login",login)
-router.get("/me",protect,getMe)
-
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 export default router;
