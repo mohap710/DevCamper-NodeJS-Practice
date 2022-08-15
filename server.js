818cbdb91489aa8e4fa2b949adcd5b63b2eb535b
mohap710 <Mohap710@gmail.com>
2022-08-15 09:51:03 +0100

    used some packages to add more security layer against common attacks( mongoSanitize xssClean helmet rateLimit hpp cors).

--- server.js
+++ server.js
@@ -14,6 +14,29 @@ import reviewRoutes from "./routes/reviews.js"
 // Middlewares
 import { errorHandler } from "./middlewares/errorHandler.js";
 
+
+// 1.Sanitize data for preventing NoSQL Injection
+import mongoSanitize from "express-mongo-sanitize";
+
+// 2. prevent XSS
+import xssClean from "xss-clean";
+
+// 3. Set Security Headers
+import helmet from "helmet";
+
+// 4. limit Rate of requests (100 request per 10min)
+import { rateLimit } from "express-rate-limit";
+const requestLimit = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max : 100
+})
+
+// 5. Prevent Params Pollution (HPP)
+import hpp from "hpp";
+
+// 6. Enable CORS ( Public APIs )
+import cors from "cors";
+
 // dirname in ES Module
 import path from "path";
 import { fileURLToPath } from "url";
@@ -27,14 +50,16 @@ const app = express()
 
 // Middlewares
 app
-.use(express.json())
-.use(fileUpload())
-.use(cookieParser())
-.use(express.static(path.join(__dirname,"/public")))
-
-
-
-const port = process.env.PORT || 5000
+  .use(express.json())
+  .use(mongoSanitize())
+  .use(xssClean())
+  .use(helmet())
+  .use(requestLimit)
+  .use(hpp())
+  .use(cors())
+  .use(fileUpload())
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, "/public")));
 
 // Mounting Routes
 
@@ -44,4 +69,5 @@ app.use("/auth", authRoutes).use(errorHandler);
 app.use("/users", userRoutes).use(errorHandler);
 app.use("/reviews", reviewRoutes).use(errorHandler);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode and listening on port ${port}!`.bgGreen.black.bold))
