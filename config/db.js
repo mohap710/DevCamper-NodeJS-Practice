d1ae65feb6b89da07129351bd451a85752dc6699
mohap710 <Mohap710@gmail.com>
2022-08-21 16:12:08 +0200

    V1 is done and ready to deploy on heroku

--- config/db.js
+++ config/db.js
@@ -1,9 +1,17 @@
 import colors from 'colors'
 import mongoose from 'mongoose'
+import dotEnv from "dotenv";
+dotEnv.config({ path: "./config/config.env" });
+
+const mongoUri =
+  process.env.NODE_ENV == "development"
+    ? process.env.MONGO_URI
+    : process.env.PROD_MONGO_URI
+
  const connectToDB = async () =>{
-     const conn = await mongoose.connect(process.env.MONGO_URI, {
-         useNewUrlParser: true,
-     })
+     const conn = await mongoose.connect(mongoUri, {
+       useNewUrlParser: true,
+     });
      console.log(`MonogoDB connected ${conn.connection.host}`.cyan.bold);
  }
  export default connectToDB;
